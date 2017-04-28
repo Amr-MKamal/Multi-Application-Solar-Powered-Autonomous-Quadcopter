3375f20408f89a063128deaef5beda948d1366f4
Amr-MKamal <thedamger_37@hotmail.com>
2017-04-29 00:27:33 +0200

    Update control.h

--- control.h
+++ control.h
@@ -6,12 +6,16 @@
 #include <sched.h>
 #include <math.h>
 #define SUCCESS 0
-#define pwm_range 255
-#define wmax 1400
-#define mass 1.5
-#define g 9.81
-#define k 206e-9
-#define motor1 1
+#define pwm_range 255 //default but can change 
+#define wmax 1400   //motor max
+#define mass 1.5 //mass 
+#define g 9.81 //gravity 
+#define k 206e-9 //f=kw^2
+#define Km // motor gain
+#define Ixx //moment of inertia calculated value
+#define Iyy
+#define Izz
+#define motor1 1  //input pins for pwm
 #define motor2 3
 #define motor3 5
 #define motor4 7
