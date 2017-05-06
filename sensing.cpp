49f6e0675b214e1b8d0f0dc604bae8d05f11ec0e
Amr-MKamal <thedamger_37@hotmail.com>
2017-05-06 16:27:01 +0200

    Create sensing.cpp

---
+++ sensing.cpp
@@ -0,0 +1,20 @@
+#include "sensing.h"
+#include "sensehat.h"
+#include "RTIMULibDrive11.h"
+#include ""
+#include "RTIMULib.h"
+double pitch , yaw , roll ; 
+double xdotdot,ydotdot, zdotdot; 
+ObtenirOrientation(float &pitch, float &roll, float & yaw);
+//functions that will be used const RTVector3& getMeasuredPose() { return m_fusion->getMeasuredPose(); }
+/*
+    const RTVector3& getGyro() { return m_imuData.gyro; }   // gets gyro rates in radians/sec
+    const RTVector3& getAccel() { return m_imuData.accel; } // get accel data in gs
+    const RTVector3& getCompass() { return m_imuData.compass; } // gets compass data in uT
+
+*/
+//get data like 
+   /* RTIMU_DATA imuData = imu->getIMUData();
+    humidite->humidityRead(imuData);
+return imuData.humidity;
+*/
