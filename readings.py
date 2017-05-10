261ab8460ef75fd25479d4abcdb6f6069ac67eb1
Amr-MKamal <thedamger_37@hotmail.com>
2017-05-10 18:51:30 +0200

    Update readings.py
    add potentials for calculating altitude and angular acceleration

--- readings.py
+++ readings.py
@@ -16,7 +16,25 @@ north = sense.get_compass()
 humidity = sense.get_humidity()
 temp = sense.get_temperature()
 pressure = sense.get_pressure()
+sea_pressure=1013.25 #in milibars
+T0=288.15
+(T0-((pressure/sea_pressure)^(0.190266436)))/0.0065=h0 
+#function for altitude as following this all could be wrong it needs data from a weather station 
+deltaH=(temp-((pressure/old_pressure)^(0.190266436)))/0.0065 +h0
+#or here https://github.com/RobertCL/ArdulogAltitudeLogger/blob/master/ardulog/ardulog.ino#L118
 #name all the above as a function called readings 
 #intalize it for one time to save data 
 #put it inside a function inside a loop and write the data to a file 
 #calculate angular accel from accel 
+#function a = acceleration(inputs, angles, xdot, m, g, k, kd)
+    gravity = [0; 0; -g];
+    R = rotation(angles);
+    T = R * thrust(inputs, k);
+    Fd = -kd * xdot;
+    a = gravity + 1 / m * T + Fd;
+end
+
+function omegadot = angular_acceleration(inputs, omega, I, L, b, k)
+    tau = torques(inputs, L, b, k);
+    omegaddot = inv(I) * (tau - cross(omega, I * omega));
+#end
