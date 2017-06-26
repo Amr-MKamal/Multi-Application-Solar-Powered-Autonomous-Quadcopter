180cc290e4100ce66c82d16e17e498737e39fe6e
Amr-MKamal <thedamger_37@hotmail.com>
2017-06-27 01:24:59 +0200

    Update sensing.h

--- sensing.h
+++ sensing.h
@@ -1,4 +1,4 @@
 #ifndef sensing_H
 #define sensing_H
-double extern pitch , roll, yaw ,xdotdot,ydotdot,zdotdot ,pitchdot,yawdot,rolldot;
+double extern  volatile pitch , roll, yaw ,xdotdot,ydotdot,zdotdot ,pitchdot,yawdot,rolldot;
 #endif
