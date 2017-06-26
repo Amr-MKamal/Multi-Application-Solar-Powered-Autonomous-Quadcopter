e6924e2fad62efc7dc5e1ecff24ebe5ce923d806
Amr-MKamal <thedamger_37@hotmail.com>
2017-06-27 01:40:35 +0200

    Update Angular_PIDS.h

--- Angular_PIDS.h
+++ Angular_PIDS.h
@@ -1,1 +1,9 @@
-
+  
+#ifndef ESC_H_INCLUDED
+#define ESC_H_INCLUDED
+  double u1 ( double z );
+  double u2 (double pitch_desired);
+  double u3 (double roll_desired);
+  double u4 (double yaw_desired);
+  double U_time (double desired_distance, double angle ) ;
+    #endif
