f1c5540bfeefd719d19e12c568236a176089d343
Amr-MKamal <thedamger_37@hotmail.com>
2017-06-27 01:51:34 +0200

    Update Angular_PIDS.h

--- Angular_PIDS.h
+++ Angular_PIDS.h
@@ -1,6 +1,12 @@
   
 #ifndef ESC_H_INCLUDED
 #define ESC_H_INCLUDED
+#define Kppr 1.4
+#define Kipr 0.05
+#define Kdpr 15
+#define Kpy 4
+#define Kiy 0.02
+#define Kdy 0
   double u1 ( double z );
   double u2 (double pitch_desired);
   double u3 (double roll_desired);
