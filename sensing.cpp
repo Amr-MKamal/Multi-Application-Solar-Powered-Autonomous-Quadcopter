c208ff4c655c1cb2cc6403865978cba0a01a5091
Amr-MKamal <thedamger_37@hotmail.com>
2017-06-29 19:22:51 +0200

    Update sensing.cpp

--- sensing.cpp
+++ sensing.cpp
@@ -1,22 +1,21 @@
-#include "sensing.h"
-#include "sensehat.h"
+
 #include<iostream>
 #include<fstream>
 #include<string>
 #include<sstream>
-#include<conio.h>
+#include <unistd.h>
+#include <stdlib.h>
 using namespace std ;
-
-double volatile pitch , yaw , roll ; 
-double volatile xdotdot,ydotdot, zdotdot; 
-double volatile pitchdot, yawdot , rolldot;
+double volatile pitch , yaw , roll ;
+double volatile xdotdot,ydotdot, zdotdot;
+double volatile pitchdot , yawdot , rolldot;
 int main()
 {
-	string myArray[7];
-	string read[16];
-    while(True)
+	string readerline;
+	 string myArray[12];
+   while(1)
     {
-        
+
 	ifstream reader("/home/pi/Desktop/readings.txt");
         //check if the file exist
 	if(!reader)
@@ -29,29 +28,29 @@ int main()
     //store reads in array while outputting, skipping blank lines
 	while(reader.good())
 	{
-	 
+
 	//add the contents of the readings.txt to an array"myArray"
-	for(int i = 0; i < 7; ++i)
+	for(int i = 0; i < 12; ++i)
         {
 			reader >> myArray[i];
+			//cout<<myArray[i]<<endl;
         }
       //use the contents of the array
-		
-		
+
+
 	}
-        yaw=strtod(read[0].c_str(),NULL);
-        roll=strtod(read[1].c_str(),NULL);
-        pitch=strtod(read[2].c_str(),NULL);
-        xdotdot=strtod(read[3].c_str(),NULL);
-	ydotdot=strtod(read[4].c_str(),NULL);
-	zdotdot=strtod(read[5].c_str(),NULL);
-        pitchdot=strtod(read[6].c_str(),NULL);
-        rolldot=strtod(read[7].c_str(),NULL);
-	yawdot=strtod(read[8].c_str(),NULL);
-        #delay for 2ms
-         time.sleep(2/1000.0);
+yaw= strtod(myArray[0].c_str(),NULL);
+roll=strtod(myArray[1].c_str(),NULL);
+pitch=strtod(myArray[2].c_str(),NULL);
+xdotdot=strtod(myArray[3].c_str(),NULL);
+ydotdot=strtod(myArray[4].c_str(),NULL);
+zdotdot=strtod(myArray[5].c_str(),NULL);
+pitchdot=strtod(myArray[6].c_str(),NULL);
+rolldot=strtod(myArray[7].c_str(),NULL);
+yawdot=strtod(myArray[8].c_str(),NULL);
+        //delay for 2ms
+         usleep(10000);
     }
 }
-
 
 
