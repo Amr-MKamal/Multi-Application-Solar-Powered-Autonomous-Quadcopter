2cd0bbf9fcd83921eda6cab35e5203ab5772aa1c
Amr-MKamal <thedamger_37@hotmail.com>
2017-06-29 19:19:26 +0200

    Update control_v2.h

--- control_v2.h
+++ control_v2.h
@@ -5,19 +5,19 @@
 #include <pigpio.h>
 #include <sched.h>
 #include <math.h>
-#define SUCCESS 0 
-#define wmax 1400   //motor max
-#define mass 1.5 //mass 
-#define g 9.81 //gravity 
-#define k 206e-9 //f=kw^2
-#define Km // motor gain
+#define SUCCESS 0
+#define wmax 16100   //motor max
+#define mass 1.5 //mass
+#define g 9.81 //gravity
+#define Kf 206e-9 //f=kw^2
+#define Km 10// motor gain
 #define Ixx //moment of inertia calculated value
 #define Iyy
 #define Izz
 #define motor1 4  //it's actually 7 https://pinout.xyz/#
 #define motor2 17 //actuall 11 , and 9 is a ground
-#define motor3 18 //actuall 12 
-#define motor4 27 // actuall 13 and 14 is a ground 
+#define motor3 18 //actuall 12
+#define motor4 27 // actuall 13 and 14 is a ground
 //sense hat pins 2,3,16,18,22,24
 void hover_q(void);
 void standard_forward(void);
